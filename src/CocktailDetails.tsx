ae7649e943fb2e8592e4d8a2074425c82387d9a2
Valentine Lejeune <valentine.lejeune1@gmail.com>
2023-03-20 11:30:26 +0100

    feat: add comments

--- src/CocktailDetails.tsx
+++ src/CocktailDetails.tsx
@@ -1,12 +1,51 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+ import { useFormik } from 'formik';
 import { Cocktail, transformCocktail } from './Cocktail.model';
+
+interface Comment {
+    comment: string;
+    user: string;
+    date: Date;
+    note: number | string;
+}
 
 const CocktailDetails = () => {
     const navigate = useNavigate();
+    const [error, setError] = useState<string>()
     const [cocktail, setCocktail] = useState<Cocktail>()
+    const [comments, setComments] = useState<Comment[]>([{comment: 'What an amazing cocktail! Love it', user: 'John Doe', date: new Date(), note: 5}, {comment: 'I don\'t like it, to much sugar', user: 'Jane Doe', date: new Date(), note: 1}])
     const id = useParams<{ id: string }>().id
+
+    const {handleSubmit, handleChange, values} = useFormik<Comment>({
+        initialValues: {
+            comment: '',
+            user: '',
+            date: new Date(),
+            note: '',
+        },
+        onSubmit: (values, {resetForm}) => {
+            if (values.note < 1 || values.note > 5) {
+                setError('Note must be between 1 and 5')
+                return
+            }
+
+            if (!values.user || !values.comment || !values.note){
+                setError('Please fill all fields')
+                return
+            }
+            
+            setError(undefined)
+            setComments(old => [{
+                comment: values.comment,
+                user: values.user,
+                date: new Date(),
+                note: values.note,
+            }, ...old])
+            resetForm()
+        },
+    });
+
     useEffect(() => {
         const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
         fetch(url)
@@ -35,6 +74,31 @@ const CocktailDetails = () => {
         <div className='m-4 md:col-span-2 lg:col-span-1'>
             <h3>Instructions ✨</h3>
             <div>{cocktail?.strInstructions}</div>
+            <div className="my-8 text-lg font-['Nova_Mono'] italic">Enjoy 🥂</div>
+        </div>
+
+        <div className="m-4">
+            <h3>Comments 🍹</h3>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 py-4 border-b">
+                <input type="text" placeholder="Your name" name="user" onChange={handleChange} value={values.user} />
+                <textarea placeholder="Your comment" name="comment" onChange={handleChange} value={values.comment} />
+                <input type="number" placeholder="Your note" name="note" onChange={handleChange} value={values.note} />
+                {error && <div className="text-red-500">{error}</div>}
+                <button type="submit">Add a comment</button>
+            </form>
+            {comments.map((comment, idx) => 
+                <div key={`${comment.comment}-${idx}`} className="border-b py-4">
+                    <div className="flex items-center">
+                        {comment.user}
+                        <p className="text-xs ml-4">{comment.date.toLocaleDateString('fr')}</p>
+                    </div>
+                    
+                    {comment.note && Array.from({ length: typeof comment.note === 'number' ? comment.note : comment.note.length }, (_, i) => (
+                        <span key={`note-${i}-${idx}-${comment.user}`}>⭐</span>
+                    ))}
+                    <div>{comment.comment}</div>
+                </div>
+            )}
         </div>
     </div>
 }
